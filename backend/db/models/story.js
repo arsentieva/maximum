1e6c0d761dcff264d5cdc36a8c39e6e95f60654d
Aaron Bruce <aaronbruce555@gmail.com>
2020-06-13 17:35:17 -0400

    Add story model and stories table

---
+++ backend/db/models/story.js
@@ -0,0 +1,28 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Story = sequelize.define('Story', {
+    title: {
+      type: DataTypes.STRING(70),
+      allowNull: false,
+    },
+    byline: {
+      type: DataTypes.STRING(140),
+      allowNull: false,
+    },
+    body: {
+      type: DataTypes.TEXT,
+    },
+    image: {
+      type: DataTypes.STRING(255),
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    }
+  }, {});
+  Story.associate = function (models) {
+    // associations can be defined here
+    Story.belongsTo( models.User, { foreignKey: 'userId' });
+  };
+  return Story;
+};
--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -16,6 +16,7 @@ module.exports = (sequelize, DataTypes) => {
   );
   User.associate = function (models) {
     // associations can be defined here
+    User.hasMany(models.Story, { foreignKey: 'userId' })
   };
   return User;
 };
