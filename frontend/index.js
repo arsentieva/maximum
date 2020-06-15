830be4555ee9b701499280945722505b28dfabe7
Luke Nicholson <61343769+lukenicholson@users.noreply.github.com>
2020-06-15 16:20:20 -0700

    Create log-in.js and log-in.pug to support log in functionality with backend (#8)
    * add .env, initial download
    
    * Run migrations and set up user
    
    * fix package-lock.json
    
    * Mange database and merge conflicts for pull request
    
    * fix sync issues with development branch
    
    * added basic log-in view and functionality to communicate with backend
    
    * add form fields for email and password in the log-in.pug

--- frontend/index.js
+++ frontend/index.js
@@ -2,12 +2,19 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
+
 app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/sign-up", (req, res) => {
   res.render("sign-up", { title: "Sign up page" });
 });
+
+app.get("/log-in", (req, res) => {
+  res.render('log-in');
+})
+
+
 
 const port = 4001;
 app.listen(port, () => {
