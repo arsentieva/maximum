481db0ea1474afb323a781c06d0722644f2d1a8c
anusca <anna.arsentieva@gmail.com>
2020-06-15 20:00:17 -0700

    add routes to get a story by id, get all stories, create a story as well as update a story

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 
 const environment = require("./config/index").environment;
 const userRouter = require("./routes/users");
+const storiesRouter = require("./routes/stories");
 
 const app = express();
 app.use(morgan("dev"));
@@ -11,13 +12,12 @@ app.use(express.json());
 app.use(cors({ origin: "http://localhost:4001" }));
 
 app.use("/users", userRouter);
-
-// Error handlers. (must have all four arguments to communicate to Express that
-// this is an error-handling middleware function)
+app.use("/stories", storiesRouter);
 
 // Process sequelize errors
 app.use((err, req, res, next) => {
   // check if error is a Sequelize error:
+  console.log(err);
   if (err instanceof ValidationError) {
     err.errors = err.errors.map((e) => e.message);
     err.title = "Sequelize Error";
