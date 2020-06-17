3c9a631965d33cd183f2201411f48b9d54832785
Aaron Bruce <aaronbruce555@gmail.com>
2020-06-17 17:17:57 -0400

    Update comments.js file

--- backend/routes/comments.js
+++ backend/routes/comments.js
@@ -21,18 +21,19 @@ router.get("/:id/comments", asyncHandler(async (req, res) => {
         order: [["createdAt", "DESC"]],
         attributes: ["id", "body", "createdAt", "updatedAt"],
     });
-    res.json({ comments });
+    const { id, body, createdAt, userId } = comments;
+    res.json({ id, body, createdAt, userId });
 }));
 
 function commentNotFoundError(id) {
-    const err = Error("Story not found");
-    err.errors = [`Story with id of ${id} could not be found`];
-    error.title = "Story not found";
+    const err = Error("Comment not found");
+    err.errors = [`Comment with id of ${id} could not be found`];
+    error.title = "Comment not found";
     err.status = 404;
     return err;
 }
 
-router.post("/:id/comments", validateComment, asyncHandler(async (req, res) => {
+router.post("/:id/comments", validateComment, handleValidationErrors, asyncHandler(async (req, res) => {
     const storyId = req.params.id;
     const { body, createdAt, userId } = req.body; // TODO Anything Else?
     const comment = await Comment.create({
@@ -44,16 +45,18 @@ router.post("/:id/comments", validateComment, asyncHandler(async (req, res) => {
     res.json({ comment });
 }));
 
-router.put("/:id/comments/:commentId", validateComment, asyncHandler(async (req, res, next) => {
-    const storyId = req.params.id;
-    const story = await Story.findOne({
-        where: { id: storyId }
-    });
+router.put("/:id/comments/:commentId", validateComment, handleValidationErrors, asyncHandler(async (req, res, next) => {
+    const userId = req.user.id;
+    // TODO Not sure if needed for processing a comment update
+    // const storyId = req.params.id;
+    // const story = await Story.findOne({
+    //     where: { id: storyId }
+    // });
     const commentId = req.params.commentId;
     const comment = await Comment.findOne({
         where: { id: commentId }
     });
-    if(req.user.id.toString() !== comment.userId.toString()) {
+    if(userId.toString() !== comment.userId.toString()) {
         const err = new Error("Unauthorized");
         err.status = 401;
         err.message = "You are not authorized to edit this comment.";
@@ -61,8 +64,8 @@ router.put("/:id/comments/:commentId", validateComment, asyncHandler(async (req,
         throw err;
     }
     if(comment) {
-        const { body, createdAt, userId } = req.body;
-        await comment.update({ body, createdAt, userId });
+        const { body } = req.body;
+        await comment.update({ body, updatedAt: new Date(), userId });
         res.json({ comment });
     } else {
         next(commentNotFoundError(commentId));
