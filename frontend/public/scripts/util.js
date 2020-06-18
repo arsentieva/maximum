dc71ca36f980be875d8652f957af5282d662a437
luke <lukenicholsontx@gmail.com>
2020-06-18 14:52:36 -0700

    commit changes left by last commitment

--- frontend/public/scripts/util.js
+++ frontend/public/scripts/util.js
@@ -1,11 +1,15 @@
 export function authorCardBuilder(User, datePublished) {
 // const authorCardBuilder = (User, datePublished) => {
   return `
-  <h4 "author-name">${User.name}</h4>
+
+
     <div class="author-image">
       <img src="/images/profile-images/1.png">
     </div>
-  <p "author-date">${datePublished}</p>
+    <div class="author-text">
+      <p "author-name">${User.name}</p>
+      <p "author-date">${datePublished}</p>
+    </div>
   `
 };
 
