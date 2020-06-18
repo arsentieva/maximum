ec3c3264a6e3bea68da81dbdd0c1e67a134ef3ae
Aaron Bruce <56603706+AaronTheBruce@users.noreply.github.com>
2020-06-18 14:06:04 -0400

    Update util.js

--- frontend/public/scripts/util.js
+++ frontend/public/scripts/util.js
@@ -9,5 +9,5 @@ export function authorCardBuilder(User, datePublished) {
   `
 };
 
-// export const backendURL = 'https://radiant-garden-26318.herokuapp.com';
-export const backendURL = 'http://localhost:8085';
+export const backendURL = 'https://radiant-garden-26318.herokuapp.com';
+// export const backendURL = 'http://localhost:8085';
