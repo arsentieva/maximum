bd1688e591386374945c718c0ed0d8192af60dda
Aaron Bruce <aaronbruce555@gmail.com>
2020-06-19 17:33:57 -0400

    Add comment post functionality

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // Define a route.
 app.get("/", (req, res) => {
-  res.render("index"); //TODO check if the index.pug file exists and it is setup
+  res.redirect("/stories");
+  // res.render("index"); //TODO check if the index.pug file exists and it is setup
 });
 
 app.get("/sign-up", (req, res) => {
@@ -24,11 +25,15 @@ app.get("/stories", (req, res) => {
 });
 
 app.get("/stories/:id(\\d+)", (req, res) => {
-  res.render("story");
+  res.render("story",{
+    id: req.params.id,
+  });
 });
 
 app.get("/stories/:id(\\d+)/comments", (req, res) => {
-  res.render("comments");
+  res.render("comments", {
+    id: req.params.id,
+  });
 });
 
 app.get("/profile", (req, res) => {
